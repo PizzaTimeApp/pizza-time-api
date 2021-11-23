f0736f3c6147d0140b7af7723fd7e8bf1a8d88e0
quentinmdl <quentin.mendel1@gmail.com>
2021-11-23 12:16:12 +0100

    feat: first model: ingredient

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Import 
 const express =  require('express');
+const path = require("path");
 
 // Initialize Server
 var server = express();
@@ -7,7 +8,8 @@ var server = express();
 // Configure Routes 
 server.get("/", function(req, res) {
     res.setHeader('Content-Type', 'text/html');
-    res.status("200").send('<h1>API - Pizza</h1>');
+    // res.status("200").send('<h1>API - Pizza</h1>');
+    res.sendFile(path.join(__dirname, "./public", "index.html"));
 })
 
 server.listen("8100", function() {
